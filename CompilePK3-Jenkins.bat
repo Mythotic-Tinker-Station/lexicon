a08525a50f9a925b879584cb281354b223747099
Mathew Antich <mifu@allfearthesentinel.net>
2018-11-29 20:32:22 +0800

    Updated compile scripts to include music folder.

--- CompilePK3-Jenkins.bat
+++ CompilePK3-Jenkins.bat
@@ -2,7 +2,7 @@
 
 set FileName=Lexicon
 
-echo Step 2: ACS
+echo Step 1: ACS
 	cd Compiler
 	bcc -acc-err-file -x bcs ..\pk3\acs\%FileName%.c ..\pk3\acs\%FileName%.o
 	if not exist ..\pk3\acs\acs.err goto 20
@@ -13,8 +13,14 @@ echo Step 2: PACK
 	cd pk3
 	..\Compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%n.pk3 *
 
+echo Step 3: PACK MUSIC AND MISC. FILES
+	cd ..
+	cd music_pk3
+	..\Compiler\7za a -r -ssw -mx9 -tzip  ..\%MusFileName%n.pk3 *
 	
-echo Step 3: REPLACE
+echo Step 4: REPLACE
 	cd ..
 	del %FileName%.pk3
 	ren %FileName%n.pk3 %FileName%-alpha-build-%BUILD_NUMBER%.pk3
+	del %MusFileName%.pk3
+	ren %MusFileName%n.pk3 %MusFileName%-alpha-build-%BUILD_NUMBER%.pk3
--- CompilePK3Fast.bat
+++ CompilePK3Fast.bat
@@ -8,13 +8,21 @@ echo Step 2: ACS
 	if not exist ..\pk3\acs\acs.err goto 20
 :20
 
-echo Step 2: PACK
+echo Step 3: PACK GAME FILES
 	cd ..
 	cd pk3
 	..\Compiler\7za a -r -ssw -mx0 -tzip  ..\%FileName%n.pk3 *
 
 	
-echo Step 3: REPLACE
+echo Step 3: PACK MUSIC AND MISC. FILES
+	cd ..
+	cd music_pk3
+	..\Compiler\7za a -r -ssw -mx0 -tzip  ..\%MusFileName%n.pk3 *
+
+	
+echo Step 4: REPLACE
 	cd ..
 	del %FileName%.pk3
 	ren %FileName%n.pk3 %FileName%.pk3
+	del %MusFileName%.pk3
+	ren %MusFileName%n.pk3 %MusFileName%.pk3

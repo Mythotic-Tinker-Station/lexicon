cf787092757b797c97573cb6117d6d684754e444
Mathew Antich <mifu@allfearthesentinel.net>
2019-01-28 15:50:02 +0800

    Updated credits and added Hellbound to acs script. No switch yet as hub map is being revamped

--- pk3/Acs/Lexicon.c
+++ pk3/Acs/Lexicon.c
@@ -50,7 +50,8 @@ strict namespace
 		{ "Deus Vult",				"DV01"	}, // 29 Slaughter Map
 		{ "Mayhem 17",				"MAY01"	}, // 30
 		{ "Sunlust",				"SLU01"	}, // 31
-		{ "Pizza Steve",			"PIZ01" }, // 32
+		{ "Pizza Steve",			"PIZ01" }, // 32 Slaughter Map
+		[ "Hellbound",				"HLB01" ], // 32
     };
     /////////////////////
     // vote manager

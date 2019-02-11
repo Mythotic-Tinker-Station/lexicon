f8d95195021390b287039d5b9c2a7aafbe3c853c
Tribeam <tribeam.3rd@gmail.com>
2019-02-10 22:15:53 -0600

    Changed the welcome message. Forgot to set the hell core switch number. Some VR map tweaks.

--- pk3/Acs/Lexicon.c
+++ pk3/Acs/Lexicon.c
@@ -402,7 +402,7 @@ strict namespace
         {
             HudSetup(0, 0);
             setfont("hudfont");
-            hudmessagebold(s:"\c[White]Welcome to the Lexicon Voting Room\n\n\c[White]This is still Work in Progress. You can follow progress on discord via\n\c[Cyan]https://discord.gg/qj9GASW"; HUDMSG_LOG, 9997, 0, hud_width_half + 0.4, 64.0, 10.0);
+            hudmessagebold(s:"\c[White]Welcome to the Lexicon\n\n\c[White]-=Alpha version=-\n\n\c[White]Please report any problems you have to our discord via\n\c[Cyan]https://discord.gg/qj9GASW"; HUDMSG_LOG, 9997, 0, hud_width_half + 0.4, 64.0, 10.0);
 
             // setup the confetti
             for(int c = 0; c < 64; c++)

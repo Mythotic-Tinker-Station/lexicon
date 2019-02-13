4a57fe67265d6bc23c00d413ef809dfe7c53ff72
Tribeam <tribeam.3rd@gmail.com>
2019-02-12 20:45:09 -0600

    Removed rail laser from votegun Added white screen flash for votegun Added classic sound for votegun

--- workshop/creditsmake/src/conf.lua
+++ workshop/creditsmake/src/conf.lua
@@ -5,7 +5,7 @@ function love.conf(t)
     t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
-    t.window.title = "offset thingy"         -- The window title (string)
+    t.window.title = "Lexicon Credits Compiler"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
     t.window.width = 800               -- The window width (number)
     t.window.height = 800               -- The window height (number)
--- workshop/creditsmake/src/main.lua
+++ workshop/creditsmake/src/main.lua
@@ -38,9 +38,7 @@ function love.load(arg)
         linepos = linepos + font:getHeight()
         
         canvas_line = love.graphics.newCanvas(font:getWidth(line), 32)
-        
-
-        
+    
         love.graphics.setCanvas(canvas_line)
 
             -- clear out previous line
@@ -49,9 +47,9 @@ function love.load(arg)
             -- reset color to white
             love.graphics.setColor(255, 255, 255, 255)
 
-            local r = line:sub(1, 1)*25
-            local g = line:sub(2, 2)*25
-            local b = line:sub(3, 3)*25
+            local r = line:sub(1, 1)*28
+            local g = line:sub(2, 2)*28
+            local b = line:sub(3, 3)*28
 
             love.graphics.setColor(r, g, b, 255)
             love.graphics.print(line:sub(4))

e6811eda91de89b676e74eb09ca714bafb8da290
Penguin <hibi414sa@gmail.com>
2019-06-02 18:09:02 +0900

    Fixed to work correctly.

--- CompileACS.bat
+++ CompileACS.bat
@@ -1,6 +1,31 @@
 @echo off
+
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+
+	goto x86_compiler
+	
+) else if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+
+	goto amd64_compiler
+
+)
+
+goto 20
+
+:x86_compiler
+echo Your CPU x86
 	cd Compiler
 	bcc -acc-err-file -x bcs ..\pk3\acs\Lexicon.c ..\pk3\acs\Lexicon.o
-	if not exist ..\pk3\acs\acs.err goto 20
-	pause
+	cd ..\
+	goto 20
+
+:amd64_compiler
+echo Your CPU AMD64
+	cd Compiler\x64
+	bcc -acc-err-file -x bcs ..\..\pk3\acs\Lexicon.c ..\..\pk3\acs\Lexicon.o
+	cd ..\..\
+	goto 20
+
 :20
+pause
+
--- CompilePK3-Jenkins.bat
+++ CompilePK3-Jenkins.bat
@@ -3,43 +3,58 @@
 set FileName=Lexicon
 set MusFileName=Lexicon-miscdata
 
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+
+	goto x86_compiler
+	
+) else if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+
+	goto amd64_compiler
+
+)
+
+:x86_compiler
+echo Your CPU x86
 echo Step 1: ACS
 	cd Compiler
-	bcc -acc-err-file -x bcs ..\pk3\acs\%FileName%.c ..\pk3\acs\%FileName%.o
-	if not exist ..\pk3\acs\acs.err goto 20
-:20
-
-if "%PROCESSOR_ARCHITECTURE%" EQU "x86" (
+	bcc -acc-err-file -x bcs ..\pk3\acs\Lexicon.c ..\pk3\acs\Lexicon.o
+	
 echo Step 2: PACK GAME FILES
 	cd ..
 	cd pk3
 	..\Compiler\7za a -r -ssw -mx8 -tzip  ..\%FileName%n.pk3 *
-)
 
-if "%PROCESSOR_ARCHITECTURE%" NEQ "ARM64" (
-echo Step 2: PACK GAME FILES
-	cd ..
-	cd pk3
-	..\Compiler\x64\7za a -r -ssw -mx8 -tzip  ..\%FileName%n.pk3 *
-)
-
-if "%PROCESSOR_ARCHITECTURE%" NEQ "x86" (
 echo Step 3: PACK MUSIC AND MISC. FILES
 	cd ..
 	cd music_pk3
 	..\Compiler\7za a -r -ssw -mx9 -tzip  ..\%MusFileName%n.pk3 *
-)
 
-if "%PROCESSOR_ARCHITECTURE%" NEQ "ARM64" (
+goto 20
+
+:amd64_compiler
+echo Your CPU AMD64
+echo Step 1: ACS
+	cd Compiler\x64
+	bcc -acc-err-file -x bcs ..\..\pk3\acs\Lexicon.c ..\..\pk3\acs\Lexicon.o
+
+echo Step 2: PACK GAME FILES
+	cd ..\..\
+	cd pk3
+	..\Compiler\x64\7za a -r -ssw -mx8 -tzip  ..\%FileName%n.pk3 *
+
 echo Step 3: PACK MUSIC AND MISC. FILES
 	cd ..
 	cd music_pk3
 	..\Compiler\x64\7za a -r -ssw -mx9 -tzip  ..\%MusFileName%n.pk3 *
-)
-	
+
+goto 20
+
+:20
 echo Step 4: REPLACE
 	cd ..
 	del %FileName%.pk3
 	ren %FileName%n.pk3 %FileName%-alpha-build-%BUILD_NUMBER%.pk3
 	del %MusFileName%.pk3
 	ren %MusFileName%n.pk3 %MusFileName%-alpha-build-%BUILD_NUMBER%.pk3
+
+

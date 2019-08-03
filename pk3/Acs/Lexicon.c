d85cb3c34db78caa6f8cbe8e486d464306b6e036
Tribeam <tribeam.3rd@gmail.com>
2019-08-03 02:00:45 -0500

    Added a script to play the original doom2 songs in order of all mapsets if the misc pk3 is missing.

--- pk3/Acs/Lexicon.c
+++ pk3/Acs/Lexicon.c
@@ -287,6 +287,12 @@ strict namespace
     // stuff that runs during any level
     script "SV_Level" open
     {
+        // check if the misc pk3 is loaded
+        if(SpawnForced("MiscPK3Loaded", 0.0, 0.0, 0.0, 0, 0) > 0) 
+        {
+            SetCVar("lexicon_global_miscpk3", 1);
+        }
+        
         // if level is the HUB map
         if(GetLevelInfo(LEVELINFO_LEVELNUM) == 99)
         {
@@ -414,12 +420,16 @@ strict namespace
             HudSetup("HUDFONT");
 
             Hudmessage(s:"\c[White]", l:"UI_MAPSET", s:"\c[Cyan]", s:votenames[GetCVar("lexicon_global_votechosen")][0], s:"\n\c[White]", l:"UI_MAP", s:"\c[Cyan]", n:PRINTNAME_LEVELNAME, s:"\n\c[White]", l:"UI_CREDITS", s:"\c[Cyan]", s:credits; HUDMSG_FADEINOUT, 8562, 0, hud_width + 0.2, hud_height - 160.0, 5.0, 1.0, 1.0);
+       
+            // run through the doom2 music in order per level
+            ACS_NamedExecute("MusicBox", 0, 0);
+        
         }
         
         //////////////////////////
         // HUB HUD init
         //////////////////////////
-        else
+        else if(GetLevelInfo(LEVELINFO_LEVELNUM) == 99)
         {
             HudSetup("HUDFONT");
             
@@ -431,6 +441,9 @@ strict namespace
                 hudmessagebold(l:"UI_COMPLETE", s:votenames[GetCVar("lexicon_global_votechosen")][0], s:"!"; 0, 9997, 0, hud_width_half + 0.4, 64.0, 30.0);
                 ACS_NamedExecute("Fireworks", 0);
             }
+            
+            // force musicbox to play doom2 intermission song
+            ACS_NamedExecute("MusicBox", 0, 33);
 
             // section names
             sectionnames[0] = strparam(s:"\c[Gold](\c[Green]", l:"UI_CAT1", s:"\c[Gold])");
@@ -678,6 +691,66 @@ strict namespace
             }
         }
     }
+    
+    script "MusicBox" (int id)
+    {
+        if(GetCVar("lexicon_global_miscpk3") == 0)
+        {
+            str track = "";
+            int lastid = GetCVar("lexicon_global_musicid");
+            
+            if(id == 0)
+            {
+                SetCVar("lexicon_global_musicid", GetCVar("lexicon_global_musicid")+1);
+                if(GetCVar("lexicon_global_musicid") > 32) { SetCVar("lexicon_global_musicid", 1); }
+                lastid++;
+            }
+            else
+            {
+                SetCVar("lexicon_global_musicid", id);
+            }
+            
+            switch(GetCVar("lexicon_global_musicid"))
+            {
+                case 1: track   = "d_runnin";   break;
+                case 2: track   = "d_stalks";   break;
+                case 3: track   = "d_countd";   break;
+                case 4: track   = "d_betwee";   break;
+                case 5: track   = "d_doom";     break;
+                case 6: track   = "d_the_da";   break;
+                case 7: track   = "d_shawn";    break;
+                case 8: track   = "d_ddtblu";   break;
+                case 9: track   = "d_in_cit";   break;
+                case 10: track  = "d_dead";     break;
+                case 11: track  = "d_stlks2";   break;
+                case 12: track  = "d_theda2";   break;
+                case 13: track  = "d_doom2";    break;
+                case 14: track  = "d_ddtbl2";   break;
+                case 15: track  = "d_runni2";   break;
+                case 16: track  = "d_dead2";    break;
+                case 17: track  = "d_stlks3";   break;
+                case 18: track  = "d_romero";   break;
+                case 19: track  = "d_shawn2";   break;
+                case 20: track  = "d_messag";   break;
+                case 21: track  = "d_count2";   break;
+                case 22: track  = "d_ddtbl3";   break;
+                case 23: track  = "d_ampie";    break;
+                case 24: track  = "d_theda3";   break;
+                case 25: track  = "d_adrian";   break;
+                case 26: track  = "d_messg2";   break;
+                case 27: track  = "d_romer2";   break;
+                case 28: track  = "d_tense";    break;
+                case 29: track  = "d_shawn3";   break;
+                case 30: track  = "d_openin";   break;
+                case 31: track  = "d_evil";     break;
+                case 32: track  = "d_ultima";   break;
+                case 33: track  = "d_read_m";   break;
+            }
+            LocalSetMusic(track);
+            SetCVar("lexicon_global_musicid", lastid);
+        }
+    }
+
 
     // sync sorted votes to clients
     script 567 (int index, int votes, int id) clientside
@@ -847,6 +920,8 @@ strict namespace
             }
             else
             {
+                // force musicbox to play first doom2 song
+                SetCVar("lexicon_global_musicid", 1);
                 TakeInventory("Lexicon_VoteGun", 0x7FFFFFFF);
                 ChangeLevel(votenames[GetCVar("lexicon_global_votechosen")][1], 0, CHANGELEVEL_NOINTERMISSION, -1);
             }

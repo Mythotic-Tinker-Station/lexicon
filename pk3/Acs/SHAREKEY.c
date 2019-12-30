4cd4e2dc07b43f4807669f58109b1534f28b918b
Tribeam <57662310+Tribeam@users.noreply.github.com>
2019-12-29 20:51:54 -0600

    Readded the log message and pickup sound for SharedKeys.

--- pk3/Acs/SHAREKEY.c
+++ pk3/Acs/SHAREKEY.c
@@ -36,12 +36,19 @@ script "SharedKey_Respawn" RESPAWN
     GetFoundKeys();
 }
 
-script "SharedKey_Pickup" (int i)
+script "SharedKey_Pickup" (int i, int snd)
 {
     if (keysfound[i] == false)
     {
         if (GetCVar("SV_SharedKeys") == 1)
         {
+            switch(snd)
+            {
+                case 0: PlaySound(0, "misc/k_pkup", CHAN_AUTO, 1.0, FALSE, ATTN_NONE); break;
+                case 1: PlaySound(0, "mayhem17/i_pkup", CHAN_AUTO, 1.0, FALSE, ATTN_NONE); break;
+            }
+            
+            Log(s:"\cd", n:0, s:"\c- ", l:UI_KEYGET, s:" \cf", s:key, s:"!");
             for (int p = 0; p < PlayerCount(); p++)
             {
                 SetActivatorToPlayer(p);

652e1923a1ab7b5d82de7a01246f22319d88d756
Popsoap <popsoap10@gmail.com>
2020-01-01 11:02:09 -0600

    Refactored shared key script to handle CVar changes

--- pk3/Acs/SHAREKEY.c
+++ pk3/Acs/SHAREKEY.c
@@ -1,65 +1,62 @@
-
-
-
 #define KEY_COUNT 3
+#define KEY_SYNC_DELAY 10
+#define PICKUP_SOUND_COUNT 2
 
-bool keysfound[KEY_COUNT];
-str keynames[KEY_COUNT] = 
+bool keysFound[KEY_COUNT];
+str keyNames[KEY_COUNT] =
 {
-    "RedCard", 
-    "YellowCard", 
+    "RedCard",
+    "YellowCard",
     "BlueCard"
 };
-
-
-function void GetFoundKeys(void)
+str pickupSounds[PICKUP_SOUND_COUNT] =
 {
-    if (GetCVar("SV_SharedKeys") == 1)
-    {
-        for (int i = 0; i < KEY_COUNT; i++)
-        {
-            if (keysfound[i] == true)
-            {
-                GiveInventory(keynames[i], 1);
-            }
-        }
-    }
-}
+    "misc/k_pkup",
+    "mayhem17/i_pkup"
+};
 
 script "SharedKey_Enter" ENTER
 {
-    GetFoundKeys();
-}
+    while (TRUE)
+    {
+        if (GetCVar("SV_ShareKeys") == 1)
+        {
+            for (int i = 0; i < KEY_COUNT; i++)
+            {
+                if (keysFound[i] == TRUE)
+                {
+                    GiveInventory(keyNames[i], 1);
+                }
+            }
+        }
 
-script "SharedKey_Respawn" RESPAWN
-{
-    GetFoundKeys();
+        Delay(KEY_SYNC_DELAY);
+    }
 }
 
 script "SharedKey_Pickup" (int i, int snd)
 {
-    if (keysfound[i] == false)
+    int alreadyFound = 1;
+
+    if (keysFound[i] == FALSE)
     {
-        if (GetCVar("SV_SharedKeys") == 1)
+        if (GetCVar("SV_ShareKeys") == 1)
         {
-            switch(snd)
-            {
-                case 0: PlaySound(0, "misc/k_pkup", CHAN_AUTO, 1.0, FALSE, ATTN_NONE); break;
-                case 1: PlaySound(0, "mayhem17/i_pkup", CHAN_AUTO, 1.0, FALSE, ATTN_NONE); break;
-            }
-            
-            Log(s:"\cd", n:0, s:"\c- ", l:"UI_KEYGET", s:" \cf", s:keynames[i], s:"!");
-            for (int p = 0; p < PlayerCount(); p++)
-            {
-                SetActivatorToPlayer(p);
-                GiveInventory(keynames[i], 1);
-            }
+            AmbientSound(pickupSounds[snd], 127);
+            Log(s:"\cd", n:0, s:"\c- ", l:"UI_KEYGET", s:" \cf", s:keyNames[i], s:"!");
+
+            GiveActorInventory(0, keyNames[i], 1);
         }
         else
         {
-            GiveInventory(keynames[i], 1);
+            ActivatorSound(pickupSounds[snd], 127);
+
+            GiveInventory(keyNames[i], 1);
         }
+
+        keysFound[i] = TRUE;
+        alreadyFound = 0;
     }
 
-    keysfound[i] = true;
+    SetResultValue(alreadyFound);
 }

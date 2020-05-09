7529b52503bc3c9955d073d532c1979b6c7799bd
Tribeam <57662310+Tribeam@users.noreply.github.com>
2020-05-09 08:09:22 -0500

    Tweaked 94tu07 fix script, actors are now angled properly, moved sector action closer to it's linedef counterpart, added a delay to the spawning.

--- pk3/Acs/94TU07_fix.c
+++ pk3/Acs/94TU07_fix.c
@@ -7,7 +7,7 @@ script 1994 enter
     if(!StrIcmp(StrParam(n:PRINTNAME_LEVEL), "94TU07"))
     {
         // spawn a actor enters sector thing in a specific spot in the map
-        Spawn("SecActEnter", -1040.0, 2656.0, -530.0, 1994);
+        Spawn("SecActEnter", -1040.0, 2288.0, -530.0, 1994);
 
         // give the sector action a...action...to execute script 1995
         Thing_SetSpecial(1994, 80, 1995, 0, 0);
@@ -19,15 +19,16 @@ script 1995 (void)
 {
     if(!nftu_activated)
     {
+        delay(35*3);
         nftu_activated = true;
-        SpawnForced("Fatso", -1504.0, 1952.0, -580.0, 0);
-        SpawnForced("Fatso", -1376.0, 2080.0, -580.0, 0);
-        SpawnForced("Fatso", -1312.0, 1824.0, -580.0, 0);
+        SpawnForced("Fatso", -1504.0, 1952.0, -580.0, 0, 32);
+        SpawnForced("Fatso", -1376.0, 2080.0, -580.0, 0, 32);
+        SpawnForced("Fatso", -1312.0, 1824.0, -580.0, 0, 32);
 
-        SpawnForced("Archvile", -1040.0, 1872.0, -580.0, 0);
+        SpawnForced("Archvile", -1040.0, 1872.0, -580.0, 0, 64);
 
-        SpawnForced("Fatso", -544.0, 1952.0, -580.0, 0);
-        SpawnForced("Fatso", -672.0, 2080.0, -580.0, 0);
-        SpawnForced("Fatso", -736.0, 1824.0, -580.0, 0);
+        SpawnForced("Fatso", -544.0, 1952.0, -580.0, 0, 96);
+        SpawnForced("Fatso", -672.0, 2080.0, -580.0, 0, 96);
+        SpawnForced("Fatso", -736.0, 1824.0, -580.0, 0, 96);
     }
 }

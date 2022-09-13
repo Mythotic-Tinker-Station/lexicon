25df6b6b5f0c8bc0ab08c85f52356e4f66c9ee45
Mathew Antich <mifu@allfearthesentinel.net>
2022-09-13 12:52:58 +0800

    Updated automata build script

--- automata.sh
+++ automata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Lexicon Main file automata script
+# Lexicon Base file automata script
 
 # Header
 echo "
 ------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------
-----------------------------------       Lexicon Main File Automata      -----------------------------------------
+----------------------------------       Lexicon Base File Automata      -----------------------------------------
 ------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------
 "
@@ -21,7 +21,6 @@ Build URL: ${BUILD_URL}
 "
 
 # Processing
-whoami
 
 # Setting Varibles
 FILE_NAME="Lexicon"
@@ -30,19 +29,10 @@ GAME_PATH='./pk3'
 
 # Compile BCS Code
 chmod +x $COMPILE_PATH/bcc
-$COMPILE_PATH/bcc -acc-err-file -x bcs $GAME_PATH/Acs/lexicon.c $GAME_PATH/Acs/Lexicon.o
+$COMPILE_PATH/bcc -acc-err-file -x bcs $GAME_PATH/ACS/main.c $GAME_PATH/ACS/main.o
 
 # Pack game files
 cd $GAME_PATH
 7za a -r -ssw -mx9 -tzip  ../${FILE_NAME}.pk3 ./*
 
-# Create proper Jenkins filenames
-cd ..
-mv ./${FILE_NAME}.pk3 ./${FILE_NAME}_beta_build_${BUILD_NUMBER}.pk3
-
-# Copy file to the lexicon zandronum folder for server use
-
-cp ./${FILE_NAME}_beta_build_${BUILD_NUMBER}.pk3 ~/.config/zandronum
-
-# Export varible to use in other scripts
-echo LEX_MAINFILE="${FILE_NAME}_beta_build_${BUILD_NUMBER}.pk3" >> lexmainfile
+echo "${FILE_NAME}.pk3"

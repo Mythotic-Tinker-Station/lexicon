b0056d7447f8034977b7b0c25fa0b478dae99d60
CandiceJoy <candice@candicejoy.com>
2022-12-24 03:47:02 -0500

    *Updated buildscripts to work on mac and linux *Added bcc compiler for mac arm64 *Added failsafes to buildscripts *Fixed Acs path capitalisation in buildscripts *Added OS/Arch detection to buildscripts

--- automata.sh
+++ automata.sh
@@ -20,6 +20,22 @@ Workspace Location: ${WORKSPACE}
 Build URL: ${BUILD_URL}
 "
 
+if ! command -v 7za &> /dev/null
+then
+    echo "7za not detected; please install 7za (for example, using brew install p7zip)"
+    exit
+fi
+
+# $ARCH is x86_64* for x64, i*86 for x86, or arm* for arm
+ARCH=$(uname -m)
+
+#$OSTYPE is linux-gnu* for linux, freebsd* for bsd, or darwin* for mac
+if [[ "$OSTYPE" == "darwin"* && $ARCH =~ ^arm ]]; then
+	BCC_COMPILER="bcc-mac"
+else
+	BCC_COMPILER="bcc"
+fi
+
 # Processing
 
 # Setting Varibles
@@ -29,10 +45,12 @@ GAME_PATH='./pk3'
 
 # Compile BCS Code
 chmod +x $COMPILE_PATH/bcc
-$COMPILE_PATH/bcc -acc-err-file -x bcs $GAME_PATH/ACS/main.c $GAME_PATH/ACS/main.o
+$COMPILE_PATH/$BCC_COMPILER -acc-err-file -x bcs $GAME_PATH/Acs/lexicon.c $GAME_PATH/Acs/lexicon.o
+[ $? -eq 0 ]  || exit 1
 
 # Pack game files
 cd $GAME_PATH
 7za a -r -ssw -mx9 -tzip  ../${FILE_NAME}.pk3 ./*
+[ $? -eq 0 ]  || exit 1
 
 echo "${FILE_NAME}.pk3"

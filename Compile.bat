d2d4068c165a67f6f42cff3be7b4760d7cbb9b06
Candice <CandiceJoy@users.noreply.github.com>
2023-01-23 05:11:31 -0600

    Updated Windows bcc.exe and build script

--- Compile.bat
+++ Compile.bat
@@ -3,20 +3,17 @@
 set FileName=Lexicon
 
 echo Step 1: ACS
-	cd compiler
-	bcc -acc-err-file -x bcs ..\pk3\acs\lexicon.c ..\pk3\acs\lexicon.o
-	if not exist ..\pk3\acs\acs.err goto 20
-	del ..\pk3\acs\acs.err
+	compiler\bcc.exe -acc-err-file -x bcs pk3\acs\lexicon.c pk3\acs\lexicon.o
+	if not exist pk3\acs\acs.err goto 20
+	del pk3\acs\acs.err
 
 :20
 echo Step 2: PACK
-	cd ..\pk3
-	..\compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%n.pk3 *.kvx *.x* *.txt *.o *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.ogg *.vgz *.wav *.pk3 *.gl
+	compiler\7za a -r -ssw -mx9 -tzip  %FileName%-New.pk3 *.kvx *.x* *.txt *.o *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.ogg *.vgz *.wav *.pk3 *.gl
 
 
 echo Step 3: REPLACE
-	cd ..
 	del %FileName%.pk3
-	ren %FileName%n.pk3 %FileName%.pk3
+	ren %FileName%-New.pk3 %FileName%.pk3
 
 pause

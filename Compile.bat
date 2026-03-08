23f5afea3e751186bcc695113cf58161319d14f2
cubebert-github <cubebertoffical@gmail.com>
2026-03-07 19:14:17 -0800

    Moved compile all options in batch file

--- Compile.bat
+++ Compile.bat
@@ -6,13 +6,13 @@ echo 3. lexicon-slaughter
 echo 4. lexicon-ultdoom
 echo 5. lexicon-dm
 echo 6. lexicon-ctf
+echo 7. all
 
-set /p firstChoice=Enter your choice (1-6): 
+set /p firstChoice=Enter your choice (1-7): 
 
 echo 1. Compile ACS
 echo 2. Pack
 echo 3. Both
-echo 4. Compile and pack everything (Very slow!)
 
 set /p secondChoice=Enter your choice (1-3): 
 
@@ -62,6 +62,37 @@ if %secondChoice%==1 (
 
 	if %firstChoice%==6 (
 		echo Step 1: ACS
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-ctf\pk3\acs\LEXCTF.acs lexicon-ctf\pk3\acs\LEXCTF.o
+		if not exist lexicon-ctf\pk3\acs\acs.err goto 20
+		del lexicon-ctf\pk3\acs\acs.err
+		pause
+	)
+
+	if %firstChoice%==7 (
+		echo Step 1: ACS
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs core\pk3\acs\Lexicon.acs core\pk3\acs\Lexicon.o
+		if not exist core\pk3\acs\acs.err goto 20
+		del core\pk3\acs\acs.err
+		compiler\acc\acc.exe core\pk3\acs\LexGUI.acs core\pk3\acs\LexGUI.o
+		if not exist core\pk3\acs\acs.err goto 20
+		del core\pk3\acs\acs.err
+
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-base\pk3\acs\LEXBASE.acs lexicon-base\pk3\acs\LEXBASE.o
+		if not exist lexicon-base\pk3\acs\acs.err goto 20
+		del lexicon-base\pk3\acs\acs.err
+
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-slaughter\pk3\acs\LEXSLGT.acs lexicon-slaughter\pk3\acs\LEXSLGT.o
+		if not exist lexicon-slaughter\pk3\acs\acs.err goto 20
+		del lexicon-slaughter\pk3\acs\acs.err
+
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-ultdoom\pk3\acs\LEXULT.acs lexicon-ultdoom\pk3\acs\LEXULT.o
+		if not exist lexicon-ultdoom\pk3\acs\acs.err goto 20
+		del lexicon-ultdoom\pk3\acs\acs.err
+
+		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-dm\pk3\acs\LEXDTHM.acs lexicon-dm\pk3\acs\LEXDTHM.o
+		if not exist lexicon-dm\pk3\acs\acs.err goto 20
+		del lexicon-dm\pk3\acs\acs.err
+
 		compiler\bcc.exe -acc-stats -acc-err-file -x bcs lexicon-ctf\pk3\acs\LEXCTF.acs lexicon-ctf\pk3\acs\LEXCTF.o
 		if not exist lexicon-ctf\pk3\acs\acs.err goto 20
 		del lexicon-ctf\pk3\acs\acs.err
@@ -137,6 +168,48 @@ if %secondChoice%==2 (
 		
 		echo Step 3: REPLACE
 		cd ..\..
+		del Lexicon-CTF.pk3
+		ren Lexicon-CTF-New.pk3 Lexicon-CTF.pk3
+		pause
+	)
+
+	if %firstChoice%==7 (
+		echo Step 2: PACK
+		cd core\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		cd ..\..\lexicon-base\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-Base-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		cd ..\..\lexicon-slaughter\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-Slaughter-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		cd ..\..\lexicon-ultdoom\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-UltDoom-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		cd ..\..\lexicon-dm\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-DM-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		cd ..\..\lexicon-ctf\pk3
+		..\..\compiler\7za a -r -ssw -mx9 -tzip  ..\..\Lexicon-CTF-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.jpg *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+		echo Step 3: REPLACE
+		cd ..\..
+		del Lexicon.pk3
+		ren Lexicon-New.pk3 Lexicon.pk3
+
+		del Lexicon-Base.pk3
+		ren Lexicon-Base-New.pk3 Lexicon-Base.pk3
+
+		del Lexicon-Slaughter.pk3
+		ren Lexicon-Slaughter-New.pk3 Lexicon-Slaughter.pk3
+
+		del Lexicon-UltDoom.pk3
+		ren Lexicon-UltDoom-New.pk3 Lexicon-UltDoom.pk3
+
+		del Lexicon-DM.pk3
+		ren Lexicon-DM-New.pk3 Lexicon-DM.pk3
+
 		del Lexicon-CTF.pk3
 		ren Lexicon-CTF-New.pk3 Lexicon-CTF.pk3
 		pause
@@ -248,9 +321,9 @@ if %secondChoice%==3 (
 		ren Lexicon-CTF-New.pk3 Lexicon-CTF.pk3
 		pause
 	)
-)
 
-if %secondChoice%==4 (
+	if %firstChoice%==7 (
+		echo This will take a minute!
 		echo Step 1: ACS
 		compiler\bcc.exe -acc-stats -acc-err-file -x bcs core\pk3\acs\Lexicon.acs core\pk3\acs\Lexicon.o
 		if not exist core\pk3\acs\acs.err goto 20
@@ -318,4 +391,5 @@ if %secondChoice%==4 (
 		del Lexicon-CTF.pk3
 		ren Lexicon-CTF-New.pk3 Lexicon-CTF.pk3
 		pause
+	)
 )
